e4f7c2325cb9063d989c0ded1de20806e3978502
Nikos Papailiou <npapa@Nikoss-MacBook-Pro.local>
2015-04-09 19:37:15 +0300

    kmeans workflow

--- asap-server/asapLibrary/operators/TF_IDF_mahout/tfidf_mahout.sh
+++ asap-server/asapLibrary/operators/TF_IDF_mahout/tfidf_mahout.sh
@@ -2,15 +2,19 @@
 export JAVA_HOME='/opt/jdk1.7.0_71/'
 input=$1
 mahout="/opt/mahout-distribution-0.9/bin/mahout"
+hadoop="/opt/hadoop-2.6.0/bin/hadoop"
+hdfs="/opt/hadoop-2.6.0/bin/hdfs"
 
 #workaround for a params problem
-hv=$(hadoop version | grep Hadoop | awk  '{print $2}')
-if [[ "$hv" > "2.5" ]]; then xmdummy="-xm sequential"; fi
+#hv=$(hadoop version | grep Hadoop | awk  '{print $2}')
+#if [[ "$hv" > "2.5" ]]; then xmdummy="-xm sequential"; fi
+
+xmdummy="-xm sequential"
 
 seqfiles="sequence_files"
-hdfs dfs -rm $seqfiles
+${hdfs} dfs -rmr $seqfiles
 #remove any previous files
-hdfs dfs -rm $output/* 2>/dev/null | wc -l | echo [PREP] Deleted $(cat) old sequence files
+${hdfs} dfs -rm $output/* 2>/dev/null | wc -l | echo [PREP] Deleted $(cat) old sequence files
 
 ${mahout} seqdirectory -i $input -o $seqfiles -c UTF-8  $xmdummy -ow 
 
